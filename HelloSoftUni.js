dd73a8ab6dcbfcae54e42abf47be11229be496d6
DanailAtanasov <106173280+DanailAtanasov@users.noreply.github.com>
2022-06-05 10:45:19 +0100

    Create HelloSoftUni.js

---
+++ HelloSoftUni.js
@@ -0,0 +1,12 @@
+// This is my first JavaScript code!
+console.log('Hello SofUni');
+
+
+var x = 'My Name ';
+var y = 'Is';
+var z = x + " " + y;
+console.log(z);
+
+
+let name = "Danail" + " " + "Atanasov";
+console.log(name);
